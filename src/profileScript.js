e0d7cfac9bec3be5ba57f6d111fd1c7efcf83be7
caiquemagalhaes <caiqueromeromagalhaes@gmail.com>
2023-06-08 22:46:04 -0300

    a

--- src/profileScript.js
+++ src/profileScript.js
@@ -22,11 +22,11 @@ function calcularIMC() {
     let estado = "";
     if (imc < 18.5) {
       estado = "Magreza";
-    } else if (imc >= 18.5 && imc <= 24.9) {
+    } else if (imc >= 18.5 && imc <= 24.99) {
       estado = "Normal";
-    } else if (imc >= 25 && imc <= 29.9) {
+    } else if (imc >= 25 && imc <= 29.99) {
       estado = "Sobrepeso";
-    } else if (imc >= 30 && imc <= 39.9) {
+    } else if (imc >= 30 && imc <= 39.99) {
       estado = "Obeso";
     } else {
       estado = "Obeso Grave";
