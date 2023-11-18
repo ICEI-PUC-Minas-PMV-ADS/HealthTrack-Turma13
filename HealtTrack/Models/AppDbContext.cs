308635279e8da9fb72c73c9e4731aa47007938ee
Caique Magalhaes <130615707+Eike94@users.noreply.github.com>
2023-11-18 18:46:53 -0300

    Alterações do projeto

--- HealtTrack/Models/AppDbContext.cs
+++ HealtTrack/Models/AppDbContext.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace HealtTrack.Models
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
--- HealtTrack/Program.cs
+++ HealtTrack/Program.cs
@@ -16,9 +16,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
- builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-   .AddEntityFrameworkStores<AppDbContext>()
-   .AddDefaultTokenProviders();
+
 
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
